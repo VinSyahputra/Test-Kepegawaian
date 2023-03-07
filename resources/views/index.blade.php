0a505af1fdea990ffe74e89e38b67ee4a27f35bf
Vinsyahputra <vinthecyborg@gmail.com>
2023-03-07 12:29:42 +0700

    update index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.index')
 @section('content')
-<a href="/login" class="btn btn-primary">LOGIN</a>
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 mx-auto d-flex my-5">
+            <a href="/dashboard" class="btn btn-primary mx-auto">MASUK KE DASHBOARD</a>
+        </div>
+    </div>
+</div>
 @endsection
